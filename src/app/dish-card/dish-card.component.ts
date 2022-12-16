f09ac07ca846acc0a401982185a2f6799c6a1ad1
PaulinaGacek <paulina.gacek.pl@gmail.com>
2022-12-16 09:02:35 +0100

    Basic app layput

---
+++ src/app/dish-card/dish-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-card',
+  templateUrl: './dish-card.component.html',
+  styleUrls: ['./dish-card.component.css']
+})
+export class DishCardComponent {
+
+}
