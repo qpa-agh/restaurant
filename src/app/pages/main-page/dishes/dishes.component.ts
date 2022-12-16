e1e64060b15172d7b146a402686cbf62248ca329
PaulinaGacek <paulina.gacek.pl@gmail.com>
2022-12-16 21:57:58 +0100

    Sum is displayed

--- src/app/pages/main-page/dishes/dishes.component.ts
+++ src/app/pages/main-page/dishes/dishes.component.ts
@@ -4,34 +4,32 @@ import { Dish } from 'src/app/services/datatypes';
 import { DishService } from 'src/app/services/dish.service';
 import { AmountChangedEvent } from 'src/app/pages/main-page/dishes/dish-card/dish-card.component';
 
-export interface BasketEntry {
-  id: number;
-  nrOfReservations: number;
-}
 @Component({
   selector: 'app-dishes',
   templateUrl: './dishes.component.html',
   styleUrls: ['./dishes.component.css'],
 })
-
 export class DishesComponent implements OnInit {
   constructor(private dishService: DishService) {}
 
   dishes: Observable<Dish[]>;
-  public basket: BasketEntry[]
+  public basket = new Map();
   public nrOfDishesInChart = 0;
-  public nrOfDishes: number;
 
   ngOnInit() {
     this.dishes = this.dishService.getDishes();
-    // let subscriber = this.dishes.subscribe((x) => this.nrOfDishes = x.length);
   }
 
   public handleReservationNrChanged(event: AmountChangedEvent) {
-    console.log(event.id, event.nrOfReservations);
-  }
-
-  ngOnDestroy(){
-
+    if (event.nrOfReservations == 0) {
+      this.basket.delete(event.id);
+    } else {
+      this.basket.set(event.id, event.nrOfReservations);
+    }
+    let sum = 0;
+    this.basket.forEach((v) => {
+      sum += v;
+    });
+    this.nrOfDishesInChart = sum;
   }
 }
